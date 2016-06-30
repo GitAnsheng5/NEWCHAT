9208f30e9b978b52fbba6b078a502a4436077255
Li <li@163.com>
2016-06-30 04:35:25 -0700

    初始化代码

---
+++ sadasd/sadasd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sadasd
+//
+//  Created by mac on 16/6/30.
+//  Copyright © 2016年 AsZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
